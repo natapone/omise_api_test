2476cf76c38da0d9308e9e55714cc63c71dc5f68
Natapone Charsombut <natapone@gmail.com>
2020-03-30 15:20:25 +0700

    read account detail

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -5,14 +5,31 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Core\Configure;
 
+
+// require_once dirname(__FILE__).'/../../vendor/omise/omise-php/lib/omise/OmiseAccount.php';
+// require_once dirname(__FILE__).'/../../vendor/omise/omise-php/lib/Omise.php';
+
+use Omise;
+use OmiseAccount;
+
+
+
 class PaymentController extends AppController {
 
+  public function initialize() {
+    parent::initialize();
+
+    define('OMISE_PUBLIC_KEY', Configure::read('App.omise.OMISE_PUBLIC_KEY'));
+    define('OMISE_SECRET_KEY', Configure::read('App.omise.OMISE_SECRET_KEY'));
+    define('OMISE_API_VERSION', Configure::read('App.omise.OMISE_API_VERSION'));
+  }
+
   public function index() {
-    printf(Configure::read('App.omise.OMISE_PUBLIC_KEY'));
-    // printf("xxxxxxx");
-    // $payment = $this->Paginator->paginate($this->Articles->find());
-    $payment = "";
-    $this->set(compact('payment'));
+
+    $account = OmiseAccount::retrieve();
+    // print_r($account);
+
+    $this->set(compact('account'));
   }
 
 
--- src/Template/Payment/index.ctp
+++ src/Template/Payment/index.ctp
@@ -1,1 +1,16 @@
 <h2>My Account</h2>
+
+<table>
+  <tr>
+    <td>Email</td>
+    <td><?= $account['email'] ?></td>
+  </tr>
+  <tr>
+    <td>Currency</td>
+    <td><?= $account['currency'] ?></td>
+  </tr>
+  <tr>
+    <td>Country</td>
+    <td><?= $account['country'] ?></td>
+  </tr>
+</table>
